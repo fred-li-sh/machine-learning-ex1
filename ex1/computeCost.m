07c0f2ed922c4c01a0750f4bff924201c973f39f
Li <fred.li@sap.com>
2020-06-26 00:38:56 +0800

    gradientDescent

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -13,7 +13,7 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-prediction = X*theta;
+prediction = X * theta;
 error = (prediction - y).^2;
 J = sum(error)/(2*m);
 
